4f4498e36f15808a148280311adf78b64115057b
Rahman Elsirafy <rahman2408@gmail.com>
2021-09-09 06:08:12 -0400

    commit tree 18e0b1b1c915998c36683f84869b354b1888a8ef

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Add your code here
+// const formData = {
+//   dogName: "Rahmans other dog",
+//   dogBreed: "IDK"
+// }
+
+// const configObj = {
+//   method: "POST",
+//   headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//   },
+//   body: JSON.stringify(formData)
+// };
+
+
+function submitData(name, email) {
+  const user = {
+    name: `${name}`,
+    email: `${email}`
+  }
+  const configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(user)
+  };
+  return fetch('http://localhost:3000/users', configObj)
+  .then(response => response.json())
+  .then(appendUser)
+  .catch(function(error){
+    const errorEl = document.createElement('h2');
+    errorEl.innerHTML = error.message; 
+    document.body.appendChild(errorEl); 
+    })
+};
+
+function appendUser(idNum) {
+  const newEl = document.createElement('h2')
+  newEl.innerHTML = idNum.id 
+  document.body.appendChild(newEl);
+}
